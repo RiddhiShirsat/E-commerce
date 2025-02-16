3db1cab8e1d6288008313fd9174b3a140e83de2c
RiddhiShirsat <riddhi.shirsat23@gmail.com>
2025-02-17 00:42:22 +0530

    newly updated

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function addToCart(item, price) {
-    alert(item + " has been added to your cart!");
-}
-
-// Smooth scrolling effect for better user experience
-document.addEventListener("DOMContentLoaded", function () {
-    let navLinks = document.querySelectorAll(".nav-link");
-    let navbarCollapse = document.querySelector(".navbar-collapse");
-
-    navLinks.forEach(function (link) {
-        link.addEventListener("click", function () {
-            if (navbarCollapse.classList.contains("show")) {
-                navbarCollapse.classList.remove("show");
-            }
-        });
-    });
-});
-
 function addToCart(name, price) {
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
